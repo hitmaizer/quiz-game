0f90cd8c8f1c918b4000a45c018bf65d18a3bbbf
hitmaizer <jose.sousalves@gmail.com>
2022-02-17 20:02:26 +0000

    API data filtered to selected data

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Theme from './theme/Theme';
 import Quote from './components/Question';
+import Data from './data.json'
 
 
 export default function App() {
+    const [allData, setAllData] = React.useState([])
+
+    React.useEffect(() => {
+        const selectedData = Data.map(item => {
+            return {
+                name: item.name, 
+                capital: item.capital, 
+                region: item.region}
+        })
+        
+        setAllData(selectedData)
+    }, [])
+
+    console.log(allData)
+
     return (
         <ThemeProvider theme={Theme}>
             <div className="page__wrapper">
