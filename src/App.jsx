43cc9116656926fbb6c7e208b3087319fa3e7732
Jose Alves <jose.sousalves@gmail.com>
2022-02-17 10:40:24 +0000

    imported + markup for theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
 
 export default function App() {
     return (
-        <div className="page__wrapper">
-            <h1>Hello mate!</h1>
-        </div>
+        <ThemeProvider>
+            <div className="page__wrapper">
+                <h1>Hello mate!</h1>
+            </div>
+        </ThemeProvider>
     )
 }
