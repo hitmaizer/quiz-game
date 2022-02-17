1460bcc13f4d3ceebe636c937c236884f3684b20
Jose Alves <jose.sousalves@gmail.com>
2022-02-17 10:41:03 +0000

    colors vars on theme file

---
+++ src/theme/Theme.js
@@ -0,0 +1,13 @@
+export default {
+    colors: {
+        white: "#ffffff",
+        offWhite: "#F2F2F2",
+        darkBlue: "#2F527B",
+        yellow: "#F9A826",
+        lightBlue: "#6066D0CC",
+        danger: "#EA8282",
+        succeed: "#60BF88",
+        grayBlue: "#1D355D"
+    },
+
+}
