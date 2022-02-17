f1dd5f4221e132e57619dc639e97fc1e034d3f22
hitmaizer <jose.sousalves@gmail.com>
2022-02-17 19:11:39 +0000

    styling answers

--- src/elements/StyledAnswer.js
+++ src/elements/StyledAnswer.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
 export default styled.button`
+    padding: 32px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 32px;
     border: solid 2px ${(props) => props.theme.colors.lightBlue};
     background-color: transparent;
     color: ${(props) => props.theme.colors.lightBlue};
     border-radius: 12px;
     width: 400px;
     height: 56px;
+    cursor: pointer;
 ` 
