326924e2b135b5b9d286148a604dd7ba1d54b42a
Jose Alves <jose.sousalves@gmail.com>
2022-02-17 14:45:32 +0000

    imported styled answer

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import StyledAnswer from '../elements/StyledAnswer';
 
 export default function Choice() {
     return (
         <div className="choice__wrapper">
-            
+            <StyledAnswer>
+                <span className="choice__order">A</span>
+                <span className="choice__text">Vietnam</span>
+            </StyledAnswer>
         </div>
     )
 }
