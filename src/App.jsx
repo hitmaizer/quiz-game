5e0f1be5b59ee80dacf8249bddd2bd6579438b40
Jose Alves <jose.sousalves@gmail.com>
2022-02-17 10:40:49 +0000

    theme props on themeprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import Theme from './theme/Theme';
 
 
 export default function App() {
     return (
-        <ThemeProvider>
+        <ThemeProvider theme={Theme}>
             <div className="page__wrapper">
                 <h1>Hello mate!</h1>
             </div>
