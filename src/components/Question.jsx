997c8732cd7775e8753eb205e162f7bdad1c2b75
hitmaizer <jose.sousalves@gmail.com>
2022-02-18 10:13:10 +0000

    added class to flag for styling

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,6 +3,9 @@ import StyledCard from '../elements/StyledCard'
 import Choice from './Choice'
 
 export default function Question(props) {
+
+    
+
     return (
         <div className="question__container">
             <h1 className="game__title">Country Quiz</h1>
@@ -11,7 +14,7 @@ export default function Question(props) {
                 <h3 className="card__title">{props.state === 1 ? "What country is this flag from?" : "test"}</h3>
                 
                 {props.state === 1 && 
-                <img src={props.currentQuestion.flag} alt=""/>}
+                <img src={props.currentQuestion.flag} alt="" className="card__flag"/>}
                 <div className="choices__wrapper flex-col">
                     <Choice currentQuestion={props.currentQuestion}/>
                     <Choice currentQuestion={props.currentQuestion}/>
