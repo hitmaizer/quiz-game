39a4f897b0bdeb3a70ea8ae325f325ed10629d76
hitmaizer <jose.sousalves@gmail.com>
2022-02-18 00:59:33 +0000

    onclick function for flag questions

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ import StyledCard from '../elements/StyledCard'
 export default function Intro(props) {
     return (
         <StyledCard className="intro__wrapper flex-col">
-            <button className="gamepicker__btn" onClick={() => props.stateChanger(1)}>
+            <button className="gamepicker__btn" onClick={props.getFlag}>
                 <span className="btn__text">Flag Game</span>
             </button>
             <button className="gamepicker__btn">
