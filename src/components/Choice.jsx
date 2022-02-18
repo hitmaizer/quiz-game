f0f030131bfbc973d76fe5b88b3382b35eded47f
hitmaizer <jose.sousalves@gmail.com>
2022-02-18 12:31:18 +0000

    onclick args

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import StyledAnswer from '../elements/StyledAnswer';
 
+
 export default function Choice(props) {
     
-
+    
     return (
         <div className="choice__wrapper">
-            <StyledAnswer className="choice__item" onClick={() => props.selectAnswer(props.text)}>
+            <StyledAnswer id={props.id} className="choice__item" onClick={() => props.selectAnswer(props.text, props.id)}>
                 <span className="choice__text">{props.text}</span>
             </StyledAnswer>
         </div>
