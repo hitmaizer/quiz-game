77a5c6f32f509f5b12176f90cdf33e753457d30a
hitmaizer <jose.sousalves@gmail.com>
2022-02-18 21:39:30 +0000

    intro with title + clickable getCapitalgame

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,13 +3,16 @@ import StyledCard from '../elements/StyledCard'
 
 export default function Intro(props) {
     return (
-        <StyledCard className="intro__wrapper flex-col">
-            <button className="gamepicker__btn" onClick={props.getFlag}>
-                <span className="btn__text">Flag Game</span>
-            </button>
-            <button className="gamepicker__btn">
-                <span className="btn__text">Capitals Game</span>
-            </button>
-        </StyledCard>
+        <>
+            <h1 className="game__title">Country Quiz</h1>
+            <StyledCard className="intro__wrapper flex-col">
+                <button className="gamepicker__btn" onClick={props.getFlag}>
+                    <span className="btn__text">Flag Game</span>
+                </button>
+                <button className="gamepicker__btn" onClick={props.getCapital}>
+                    <span className="btn__text">Capitals Game</span>
+                </button>
+            </StyledCard>
+        </>
     )
 }
