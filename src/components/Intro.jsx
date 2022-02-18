d91584d654bfafe229f3d60dfc9e90c83c95f4fb
hitmaizer <jose.sousalves@gmail.com>
2022-02-18 00:27:23 +0000

    jsx markup of intro component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import StyledCard from '../elements/StyledCard'
+
+export default function Intro(props) {
+    return (
+        <StyledCard className="intro__wrapper flex-col">
+            <button className="gamepicker__btn" onClick={() => props.stateChanger(1)}>
+                <span className="btn__text">Flag Game</span>
+            </button>
+            <button className="gamepicker__btn">
+                <span className="btn__text">Capitals Game</span>
+            </button>
+        </StyledCard>
+    )
+}
