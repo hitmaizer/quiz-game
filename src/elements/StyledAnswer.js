a3c5934c8f6a89c98940f409c8d6b7ea17aff587
hitmaizer <jose.sousalves@gmail.com>
2022-02-18 20:53:34 +0000

    removed fixed color style

--- src/elements/StyledAnswer.js
+++ src/elements/StyledAnswer.js
@@ -6,16 +6,10 @@ export default styled.button`
     flex-direction: row;
     align-items: center;
     gap: 32px;
-    border: solid 2px ${(props) => props.theme.colors.lightBlue};
     background-color: transparent;
-    color: ${(props) => props.theme.colors.lightBlue};
     border-radius: 12px;
     width: 400px;
     height: 56px;
     cursor: pointer;
-    &:hover {
-        background-color: ${(props) => props.theme.colors.yellow};
-        border: solid 2px ${(props) => props.theme.colors.yellow};
-        color: ${(props) => props.theme.colors.white};
-    }
+    
 ` 
