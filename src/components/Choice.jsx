677f92408b404a2d9b8e0e44d7c103a81285ef6b
hitmaizer <jose.sousalves@gmail.com>
2022-02-18 20:53:08 +0000

    style props for conditional style

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -5,9 +5,14 @@ import StyledAnswer from '../elements/StyledAnswer';
 export default function Choice(props) {
     
     
+    
     return (
         <div className="choice__wrapper">
-            <StyledAnswer id={props.id} className="choice__item" onClick={() => props.selectAnswer(props.text, props.id)}>
+            <StyledAnswer 
+            id={props.id} 
+            className="choice__item" 
+            onClick={() => props.selectAnswer(props.text, props.id)}
+            style={props.style.style}>
                 <span className="choice__text">{props.text}</span>
             </StyledAnswer>
         </div>
