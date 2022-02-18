3947a840d5ba7d6a48f5d8f5ff354b71e25dcb57
hitmaizer <jose.sousalves@gmail.com>
2022-02-18 00:27:07 +0000

    accepting props, dynamic render of choice txt

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import StyledAnswer from '../elements/StyledAnswer';
 
-export default function Choice() {
+export default function Choice(props) {
     return (
         <div className="choice__wrapper">
             <StyledAnswer>
                 <span className="choice__order">A</span>
-                <span className="choice__text">Vietnam</span>
+                <span className="choice__text">{props.currentQuestion.country}</span>
             </StyledAnswer>
         </div>
     )
