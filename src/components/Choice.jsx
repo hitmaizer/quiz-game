7e13964a921af084971b613de875a439ed0f0d25
hitmaizer <jose.sousalves@gmail.com>
2022-02-20 10:50:57 +0000

    conditional rendering of icon div on state 3

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import StyledAnswer from '../elements/StyledAnswer';
-
+import { CheckCircle } from '@styled-icons/bootstrap/CheckCircle'
+import { XCircle } from '@styled-icons/bootstrap/XCircle'
+import { nanoid } from 'nanoid'
 
 export default function Choice(props) {
     
@@ -14,6 +16,12 @@ export default function Choice(props) {
             onClick={() => props.selectAnswer(props.text, props.id)}
             style={props.style.style}>
                 <span className="choice__text">{props.text}</span>
+                {props.state === 3 && 
+                    <div className="answer__icon">
+                        <CheckCircle size="24px"  /> 
+                        <XCircle size="24px" />
+                    </div>
+                }
             </StyledAnswer>
         </div>
     )
